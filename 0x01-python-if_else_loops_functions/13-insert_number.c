11e9fbe5ba57c8660f36a4631fdfdf1d0a8a09db
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-03 15:30:09 +0000

    inserts in sorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
@@ -10,39 +9,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *newnode, *prev, *curr;
+	listint_t *newnode, *curr;
 
-	prev = curr = *head;
+	curr = *head;
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 	{
-		free(newnode);
 		return (NULL);
 	}
 	newnode->n = number;
 	newnode->next = NULL;
-	if (*head == NULL)
+	if (curr == NULL || curr->n > number)
 	{
-		curr->next = newnode;
+		newnode->next = curr;
+		*head = newnode;
 		return (newnode);
 	}
-	if (newnode->n < curr->n)
-	{
-		newnode->next = curr->next;
-		curr->next = newnode;
-	}
-	curr = curr->next;
-	while (curr != NULL)
-	{
-		if ((curr->n > newnode->n) && ((prev->n == newnode->n) || (prev->n < newnode->n)))
-		{
-			newnode->next = curr;
-			prev->next = newnode;
-			return (newnode);
-		}
+	while  (curr->next && curr->next->n < number)
 		curr = curr->next;
-		prev = prev->next;
-	}
+	newnode->next = curr->next;
 	curr->next = newnode;
 	return (newnode);
 }
