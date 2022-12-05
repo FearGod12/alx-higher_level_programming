665053028502921f9145049ca594ce14a4536885
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-05 15:32:46 +0000

    file 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     rows = len(matrix)
     columns = len(matrix[0])
-    for i in range(rows):
-        for j in range(columns):
-            if j == columns - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=" ")
-    print()
+    if columns != 0:
+        for i in range(rows):
+            for j in range(columns):
+                if j == columns - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+    else:
+        print()
