8bceaedfaca432181297c814a502dd2cd239dd4c
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-05 19:52:34 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: double pointer to the first node of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head;
+	int len = 0, l, h;
+
+	if (*head == NULL)
+		return (1);
+	while (temp != 	NULL)
+	{
+		temp = temp->next;
+		len++;
+	}
+	int data[len];
+
+	temp = *head;
+	h = len - 1;
+	len = l = 0;
+	while (temp != NULL)
+	{
+		data[len] = temp->n;
+		temp = temp->next;
+		len++;
+	}
+	while (h > l)
+	{
+		if (data[h] != data[l])
+		{
+			return (0);
+		}
+		h--;
+		l++;
+	}
+	return (1);
+
+}
