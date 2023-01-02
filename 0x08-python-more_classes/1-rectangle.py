cca5901983210d6a536f4abaa7e3a23e7241f585
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-02 16:39:57 +0000

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" contains Class Rectabgle with
+instance attribut : width and height
+property setters :def width(self, value), def height(self, value)
+property getters : def width(self), def height(self)
+"""
+
+
+class Rectangle:
+    """Rectangle class with instance methods
+    getters- width() and height()
+    setters - height(value) and width(value)
+    where values must be an integer greater than 0
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__heigth = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self__height
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
