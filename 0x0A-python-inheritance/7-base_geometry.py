8285097bcf28e6135ba1a98943a23f93561ee67e
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-09 14:43:53 +0000

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''contains a Public instance method: def area(self): that raises an
+Exception and Public instance method
+def integer_validator(self, name, value): that validates value'''
+
+
+class BaseGeometry:
+    '''method area() raises an exception
+    method def integer_validator(self, name, value): validates value'''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
