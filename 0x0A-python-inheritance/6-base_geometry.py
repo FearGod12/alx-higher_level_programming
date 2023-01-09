385bb20248a5f7ecbc5519eef010a06b97a96333
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-09 13:51:57 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pythonb3
+'''contains a Public instance method: def area(self): that raises an
+Exception with the message area() is not implemented'''
+
+
+class BaseGeometry:
+    '''method area() raises an exception'''
+
+    def area(self):
+        raise Exception("area() is not implemented")
