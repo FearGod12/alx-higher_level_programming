1b1f3b6d48f5ccc148076c9e85f900add3d47a2d
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-13 13:32:59 -0800

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''contains the implementation of a class -
 :Base with its attributes and methods
 '''
+import json
 
 
 class Base:
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "\"[]\""
+        else:
+            return json.dumps(list_dictionaries)
