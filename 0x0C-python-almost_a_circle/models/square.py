ed7d4e3a06b42da72d99b614e90a1a7997bf535b
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-13 12:38:47 -0800

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,4 +35,25 @@ class Square(Rectangle):
         __str__ method
         """
         return "[Square] ({}) {}/{} - {}".format(
-        self.id, self.x, self.y, self.width)
+         self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        '''1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+        **kwargs must be skipped if *args exists and is not empty
+        Each key in this dictionary represents an attribute to the
+        instance'''
+
+        if args != ():
+            self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
