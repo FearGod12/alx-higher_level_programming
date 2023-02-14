4588706e7253d7590f1b6a9c30dbf681fcd023b7
FearGod12 <onyenikechukwudi@gmail.com>
2023-02-14 18:43:09 +0000

    task 5 and 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- lists all rows of the table first_table from the database hbtn_0c_0
+-- All fields will be printed
+-- The database name will be passed as an argument of the mysql
+SELECT * FROM first_table
