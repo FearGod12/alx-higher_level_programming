b78149345dc0cbfe461c2420dc355d1f2431bb89
FearGod12 <onyenikechukwudi@gmail.com>
2023-02-16 13:19:06 -0800

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id
