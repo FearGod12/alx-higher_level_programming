c601885706982d913331c2930585aaa0a5557e21
FearGod12 <onyenikechukwudi@gmail.com>
2023-03-13 20:05:45 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv[2] * 1;
+if (!isNaN(arg)) {
+  const string = 'x';
+  for (let i = 0; i < arg; i++) {
+    console.log(string.repeat(arg));
+  }
+} else {
+  console.log('Missing size');
+}
