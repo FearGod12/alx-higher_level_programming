2a2487541ddc62d8380c2e98f1e4214b9471768b
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-02 13:19:05 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter'''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    r = requests.post(url, data={'q': q})
+    try:
+        converted = r.json()
+        if len(converted) == 0:
+            print("No result")
+        else:
+            the_id = converted.get('id')
+            the_name = converted.get('name')
+            print("[{}] {}".format(the_id, the_name))
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
