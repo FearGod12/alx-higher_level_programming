52f772df772cd7e5531b53d7f7b18dbb8d364029
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-25 19:13:18 +0000

    task fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,14 +5,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    const errorMessage = {
-      Error: err.message,
-      errno: err.errno,
-      code: err.code,
-      syscall: err.syscall,
-      path: err.path
-    };
-    console.error(errorMessage);
+    console.error(err);
   } else {
     console.log(data);
   }
